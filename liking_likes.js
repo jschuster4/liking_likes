4030643d14c80b34c5707441d5c3a0a2907fa3d9
jschuster4 <jspowerex@gmail.com>
2022-04-26 14:32:55 -0500

    liking likes

---
+++ liking_likes.js
@@ -0,0 +1,10 @@
+function add1(){
+    document.querySelector(".likescount1").innerHTML++
+}
+function add2(){
+    document.querySelector(".likescount2").innerHTML++
+}
+function add3(){
+    document.querySelector(".likescount3").innerHTML++
+}
+
